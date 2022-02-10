24ec80d45df238a21828566d008df0073947581a
marco76tv <marco.sottana@gmail.com>
2022-02-10 19:21:12 +0100

    .

---
+++ Providers/CmsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Cms\Providers;
+
+use Modules\Xot\Providers\XotBaseServiceProvider;
+use Modules\Xot\Services\BladeService;
+
+/**
+ * Undocumented class.
+ */
+class CmsServiceProvider extends XotBaseServiceProvider {
+    protected string $module_dir = __DIR__;
+
+    protected string $module_ns = __NAMESPACE__;
+
+    public string $module_name = 'cms';
+
+    public function bootCallback(): void {
+        BladeService::registerComponents($this->module_dir.'/../View/Components', 'Modules\\Cms');
+    }
+
+    public function registerCallback(): void {
+    }
+}
