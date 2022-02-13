c1e12ae3a62f350a3a0a92cd0a2c7a0c4856effa
marco xot <marco.sottana@gmail.com>
2022-02-13 20:17:55 +0100

    .

--- Routes/api.php
+++ Routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/cms', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get(
+    '/cms', function (Request $request) {
+        return $request->user();
+    }
+);
