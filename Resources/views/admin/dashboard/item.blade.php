5420a50c42285b8e8d971ec6484eba9507d9b3c3
marco76tv <marco.sottana@gmail.com>
2022-09-13 11:37:28 +0200

    up

--- Resources/views/admin/dashboard/item.blade.php
+++ Resources/views/admin/dashboard/item.blade.php
@@ -1,8 +1,16 @@
 <div class="col-4">
     <x-card>
-        <x-slot name="title">CMS</x-slot>
+        <x-slot name="title">chart blade</x-slot>
         <x-slot name="txt">
             <x-chart url=""></x-chart>
+        </x-slot>
+    </x-card>
+</div>
+<div class="col-4">
+    <x-card>
+        <x-slot name="title">chart livewire</x-slot>
+        <x-slot name="txt">
+            <livewire:chart url=""></livewire:chart>
         </x-slot>
     </x-card>
 </div>
