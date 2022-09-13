b5c53adc42cf74a676bc6245130cad6d79e86592
marco76tv <marco.sottana@gmail.com>
2022-09-13 11:09:15 +0200

    up

--- Resources/views/admin/dashboard/item.blade.php
+++ Resources/views/admin/dashboard/item.blade.php
@@ -1,1 +1,8 @@
-<!-- dashboard item -->
+<div class="col-4">
+    <x-card>
+        <x-slot name="title">CMS</x-slot>
+        <x-slot name="txt">
+            <x-chart url=""></x-chart>
+        </x-slot>
+    </x-card>
+</div>
