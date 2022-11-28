f515f47c637d6ea19a63475ad78a78b533ccb2de
marco xot <marco.sottana@gmail.com>
2022-11-28 19:58:45 +0100

    up

--- Models/Panels/Policies/_ModulePanelPolicy.php
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Models\Panels\Policies;
 
+use Modules\Xot\Contracts\UserContract;
+use Modules\Xot\Contracts\PanelContract;
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 class _ModulePanelPolicy extends XotBasePanelPolicy {
+
+    public function showModelsModuleMenu(UserContract $user,PanelContract $panel):bool{
+        
+        return false;
+    }
 }
