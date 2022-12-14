3fc797c0324b1301f57c912a1a563bc48dbad0c0
marco xot <marco.sottana@gmail.com>
2022-12-14 12:44:46 +0100

    .

--- docs/navigation.php
+++ docs/navigation.php
@@ -3,14 +3,72 @@
 declare(strict_types=1);
 
 return [
-    'Getting Started' => [
-        'url' => 'docs/getting-started',
+    'Introduzione' => [
+        'url' => '#',
         'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
+            'Introducendo '.$moduleName => [
+                'url' => 'docs/introduzione',
+            ],
+            /*'Crediti' => [
+                'url' => 'docs/crediti',
+            ],*/
+            'Installazione' => [
+                'url' => 'docs/installazione',
+            ],
+            /*$moduleName.' Starter Kit' => [
+                'url' => 'docs/starter-kit',
+            ],*/
+            'Aggiornamenti' => [
+                'url' => 'docs/aggiornamenti',
+            ],
+            /*'Tabella di Marcia' => [
+                'url' => 'docs/tabella-di-marcia',
+            ],
+            'Comparazioni' => [
+                'url' => 'docs/comparazioni',
+            ],*/
         ],
     ],
-    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+    'Metodi Principali' => [
+        'url' => '#',
+        'children' => [
+            'Funzionalità 1' => [
+                'url' => 'docs/methods/',
+            ],
+            'Funzionalità 2' => [
+                'url' => 'docs/methods/',
+            ],
+        ],
+    ],
+    'Componenti' => [
+        'url' => '#',
+        'children' => [
+            'Chartjs' => [
+                'url' => '#',
+                'children' => [
+                    'Base' => [
+                        'url' => 'docs/components/chartjs/base',
+                    ],
+                ],
+            ],
+            'Graph' => [
+                'url' => 'docs/components/graph',
+            ],
+        ],
+        'Componenti 2' => [
+            'url' => 'docs/components/',
+        ],
+    ],
+
+    'Caratteristiche Avanzate' => [
+        'url' => '#',
+        'children' => [
+            'Echarts' => [
+                'url' => 'docs/advanced/echarts',
+            ],
+            'Caratteristiche Avanzate 2' => [
+                'url' => 'docs/advanced/',
+            ],
+        ],
+    ],
 ];
