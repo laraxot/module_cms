ff08576585a459bc078bfe6ce61530a3bfdb627b
marco76tv <marco.sottana@gmail.com>
2022-12-14 13:55:01 +0100

    .

---
+++ Config/xra.php
@@ -0,0 +1,11 @@
+<?php
+
+ return array (
+  'adm_theme' => 'AdminLTE',
+  'enable_ads' => '1',
+  'main_module' => 'xot',
+  
+  'primary_lang' => 'it',
+  'pub_theme' => 'BsItalia',
+  'home'=>'segnalazione-disservizio.elenco',
+);
