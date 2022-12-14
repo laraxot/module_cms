189f48aaadeccbe6e89e5eb1a7b07540563a7347
marco76tv <marco76tv@users.noreply.github.com>
2022-12-14 13:07:54 +0000

    Fix styling

--- Config/xra.php
+++ Config/xra.php
@@ -1,11 +1,13 @@
 <?php
 
- return array (
-  'adm_theme' => 'AdminLTE',
-  'enable_ads' => '1',
-  'main_module' => 'xot',
-  
-  'primary_lang' => 'it',
-  'pub_theme' => 'BsItalia',
-  'home'=>'segnalazione-disservizio.elenco',
-);
+declare(strict_types=1);
+
+ return [
+     'adm_theme' => 'AdminLTE',
+     'enable_ads' => '1',
+     'main_module' => 'xot',
+
+     'primary_lang' => 'it',
+     'pub_theme' => 'BsItalia',
+     'home' => 'segnalazione-disservizio.elenco',
+ ];
