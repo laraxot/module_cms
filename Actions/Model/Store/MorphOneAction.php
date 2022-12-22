cab3ad8cc812777d1556546f02d0c7bb0f043e5f
marco xot <marco.sottana@gmail.com>
2022-12-22 09:40:48 +0100

    up

---
+++ Actions/Model/Store/MorphOneAction.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Cms\Actions\Model\Store;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\QueueableAction\QueueableAction;
+
+class MorphOneAction {
+    use QueueableAction;
+
+    public function __construct() {
+    }
+
+    public function execute(Model $row, object $relation) {
+        if (isJson($relation->data)) {
+            $relation->data = json_decode($relation->data, true);
+        }
+        $rows = $relation->rows;
+        if ($rows->exists()) {
+            // dddx('SI');
+            $rows->update($relation->data);
+        } else {
+            // dddx($relation->data);
+            $rows->create($relation->data);
+        }
+    }
+}
