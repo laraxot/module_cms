2ac66a4558ad376122998b0620fff1c93848bc20
Davide Cavallini <davidecavallini1987@gmail.com>
2022-12-29 17:28:50 +0100

    .

---
+++ Helpers/Helper.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Collection;
+use Modules\Cms\Services\PanelService;
+
+if (! function_exists('getModuleModelsMenu')) {
+    function getModuleModelsMenu(string $module): Collection {
+        $models = getModuleModels($module);
+        $menu = collect($models)->map(
+            function ($item, $key) {
+                // $obj = new $item();
+                $obj = app($item);
+                $panel = PanelService::make()->get($obj);
+
+                if ('media' === $key) {// media e' singolare ma anche plurale di medium
+                    $panel->setName('medias');
+                }
+
+                $url = $panel->url('index');
+
+                return (object) [
+                    'title' => $key,
+                    'url' => $url,
+                    'active' => false,
+                ];
+            }
+        );
+
+        return $menu;
+    }
+}
