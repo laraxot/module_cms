f0bf6dc310ca1eface4ba293ea15c1400a382a7f
marco76tv <marco.sottana@gmail.com>
2023-01-11 13:54:49 +0100

    up

---
+++ Datas/NavbarMenuData.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Cms\Datas;
+
+use Spatie\LaravelData\Data;
+
+class NavbarMenuData extends Data {
+    public string $title;
+    public string $url;
+    public bool $active;
+}
---
+++ View/Composers/ThemeComposer.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Cms\View\Composers;
+
+use Modules\Cms\Actions\Module\GetModelsMenuByModuleNameAction;
+use Modules\Cms\Datas\NavbarMenuData;
+use Modules\LU\Services\ProfileService;
+use Modules\UI\Models\Menu;
+use Spatie\LaravelData\DataCollection;
+
+class ThemeComposer {
+    /**
+     * ---.
+     */
+    public function getArea(): ?string {
+        $params = getRouteParameters();
+        if (isset($params['module'])) {
+            return $params['module'];
+        }
+
+        return null;
+    }
+
+    public function getModelsMenuByModuleName(?string $module_name = null): DataCollection {
+        if (null == $module_name) {
+            $module_name = $this->getArea();
+        }
+        $res = app(GetModelsMenuByModuleNameAction::class)->execute($module_name);
+
+        return $res;
+    }
+
+    public function getModuleMenuByModuleName(?string $module_name = null): DataCollection {
+        if (null == $module_name) {
+            $module_name = $this->getArea();
+        }
+        $menu_name = 'module_'.$module_name;
+        // $menu = Menu::where('name', '=', $menu_name)->first();
+        $menu = Menu::firstOrNew(
+            ['name' => $menu_name]
+        );
+        // $menus = Menu::get();
+        // dddx($menus);
+        $items = $menu->items;
+
+        return NavbarMenuData::collection([]);
+    }
+
+    public function getDashboardMenu(): DataCollection {
+        $profile = ProfileService::make();
+        $menu = $profile->areas()->map(function ($item) {
+            return [
+                'title' => $item->area_define_name,
+                'url' => $item->url,
+                'active' => (bool) $item->active,
+            ];
+        });
+
+        return NavbarMenuData::collection($menu);
+    }
+}
