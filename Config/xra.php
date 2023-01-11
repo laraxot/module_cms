e0ea220646f73cfc97ef10a6040fa326a9513bda
marco76tv <marco.sottana@gmail.com>
2023-01-11 13:54:49 +0100

    up

--- Config/xra.php
+++ Config/xra.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'adm_theme' => 'AdminLTE',
     'enable_ads' => '1',
-    'main_module' => 'Quaeris',
+    'main_module' => 'PFed',
 
     'primary_lang' => 'it',
     'pub_theme' => 'One',
