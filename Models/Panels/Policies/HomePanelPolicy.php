871391c96226f6fc44f0d285dd2582912bf18f08
marco76tv <marco76tv@users.noreply.github.com>
2023-01-17 11:03:29 +0000

    Fix styling

--- Models/Panels/Policies/HomePanelPolicy.php
+++ Models/Panels/Policies/HomePanelPolicy.php
@@ -1,11 +1,8 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Modules\Cms\Models\Panels\Policies;
-
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\Cms\Models\Panels\Policies\HomePanelPolicy as Post; 
-
-use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
 class HomePanelPolicy extends XotBasePanelPolicy {
 }
