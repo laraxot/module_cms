156ba4f6dba5b731bf0e55d702529e3b6fad18e6
Marco <marco.sottana@gmail.com>
2023-01-18 12:30:53 +0100

    up

--- Actions/Panel/HomeAction.php
+++ Actions/Panel/HomeAction.php
@@ -7,13 +7,16 @@ namespace Modules\Cms\Actions\Panel;
 use Modules\Cms\Contracts\PanelContract;
 use Spatie\QueueableAction\QueueableAction;
 
-class HomeAction {
+class HomeAction
+{
     use QueueableAction;
 
-    public function __construct() {
+    public function __construct()
+    {
     }
 
-    public function execute(PanelContract $panel, array $data): PanelContract {
+    public function execute(PanelContract $panel, array $data): PanelContract
+    {
         return $panel;
     }
 }
--- Models/Panels/Policies/HomePanelPolicy.php
+++ Models/Panels/Policies/HomePanelPolicy.php
@@ -4,5 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Models\Panels\Policies;
 
-class HomePanelPolicy extends XotBasePanelPolicy {
+class HomePanelPolicy extends XotBasePanelPolicy
+{
 }
