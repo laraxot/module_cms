40bb3b3d0eac7588080665aed1fd6c9cf5988b15
marco76tv <marco76tv@users.noreply.github.com>
2023-01-25 14:00:57 +0000

    Fix styling

--- Actions/Panel/StoreAction.php
+++ Actions/Panel/StoreAction.php
@@ -7,16 +7,13 @@ namespace Modules\Cms\Actions\Panel;
 use Modules\Cms\Contracts\PanelContract;
 use Spatie\QueueableAction\QueueableAction;
 
-class StoreAction
-{
+class StoreAction {
     use QueueableAction;
 
-    public function __construct()
-    {
+    public function __construct() {
     }
 
-    public function execute(PanelContract $panel, array $data): PanelContract
-    {
+    public function execute(PanelContract $panel, array $data): PanelContract {
         $row = $panel->getRow();
 
         $rules = [];
