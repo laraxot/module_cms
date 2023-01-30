2d22b9aac88d93dcc617bf2fae7380463d10dc06
marco76tv <marco.sottana@gmail.com>
2023-01-30 16:38:32 +0100

    up

--- Actions/Panel/UpdateAction.php
+++ Actions/Panel/UpdateAction.php
@@ -20,6 +20,18 @@ class UpdateAction {
         $rules = $panel->rules(['act' => 'edit']);
         $act = str_replace('\Panel\\', '\Model\\', __CLASS__);
         $act = str_replace('\Cms\\', '\Xot\\', $act);
+
+        $parent = $panel->getParent();
+        if (null != $parent) {
+            $rows = $panel->rows;
+            if (method_exists($rows, 'getForeignKeyName') && method_exists($rows, 'getParentKey')) {
+                $foreign_key_name = $rows->getForeignKeyName();
+                $parent_key = $rows->getParentKey();
+                $data[$foreign_key_name] = $parent_key;
+            } else {
+            }
+        }
+
         app('\\'.$act)->execute($row, $data, $rules);
 
         return $panel;
