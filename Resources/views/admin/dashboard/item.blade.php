43ebeccb6cddf44b21c28469797835b017efbd82
marco76tv <marco.sottana@gmail.com>
2023-02-03 17:13:52 +0100

    .

--- Resources/views/admin/dashboard/item.blade.php
+++ Resources/views/admin/dashboard/item.blade.php
@@ -8,15 +8,15 @@
     </x-card>
 </div>
 --}}
-<div class="col-4">
+{{-- <div class="col-4">
     <x-card>
         <x-slot name="title">Cms</x-slot>
         <x-slot name="txt">
-            {{--
+            
             <livewire:graph url="/api/xxx/answers" id="graph1"></livewire:graph>
 
             <x-graph url="/api/xxx/answers" id="graph1"></x-graph>
-            --}}
+           
         </x-slot>
     </x-card>
-</div>
+</div> --}}
