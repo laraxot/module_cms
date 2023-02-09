b3c996d5a71e7e76c3c1f618be690541ec0bc4f3
marco76tv <marco.sottana@gmail.com>
2023-02-09 17:31:53 +0100

    up

--- Http/Controllers/Admin/ConfController.php
+++ Http/Controllers/Admin/ConfController.php
@@ -7,6 +7,7 @@ namespace Modules\Cms\Http\Controllers\Admin;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Modules\Cms\Actions\GetViewAction;
 use Modules\Cms\Http\Controllers\BaseController;
 use Modules\Settings\Services\ConfService;
 use Modules\Tenant\Services\TenantService;
@@ -44,7 +45,11 @@ class ConfController extends BaseController {
                 // ->with('row',$row)
         ;
         */
-        $view = $this->panel->getView();
+        $view = app(GetViewAction::class)->execute();
+        $view_params = [
+            'rows' => $rows,
+            // 'row'=>$row,
+        ];
 
         return view($view, $view_params);
     }
@@ -60,7 +65,7 @@ class ConfController extends BaseController {
         $row = config($item0);
 
         // return ThemeService::v1iew()->with('row', $row);
-        $view = $this->panel->getView();
+        $view = app(GetViewAction::class)->execute();
         $view_params = [
             'row' => $row,
         ];
