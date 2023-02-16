939d2c0a1e6cc212a4d3581da9688fcac9636e22
marco76tv <marco76tv@users.noreply.github.com>
2023-02-16 11:31:49 +0000

    Fix styling

--- Actions/GetStyleClassByViewAction.php
+++ Actions/GetStyleClassByViewAction.php
@@ -13,11 +13,11 @@ class GetStyleClassByViewAction {
     public function execute(string $view = ''): string {
         $config_path = inAdmin() ? 'adm_theme' : 'pub_theme';
         $config_key = '::'.Str::after($view, '::components.').'.class';
-        $key=$config_path.$config_key;
-        $key1='ui'.$config_key;
+        $key = $config_path.$config_key;
+        $key1 = 'ui'.$config_key;
         $class = config($key) ?? config($key1);
         if (! is_string($class)) {
-            dddx(['kye'=>config($key),'k1'=>config($key1)]);
+            dddx(['kye' => config($key), 'k1' => config($key1)]);
             throw new \Exception('create config ['.$key.']['.$key1.']'.__LINE__.']['.__FILE__.']');
         }
 
