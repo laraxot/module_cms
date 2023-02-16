8769d1f8728238e17d672ccb1b1c436a5882ea90
Retype GitHub Action <retype@object.net>
2023-02-16 19:13:20 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/panel/crud/v1.blade.php
+++ resources/views/components/panel/crud/v1.blade.php
@@ -48,4 +48,8 @@
         </tbody>
     </table>
 </div>
+<div class="card-footer border-0 py-5">
+    <span class="text-muted text-sm">Showing {{ $rows->count() }} items out of {{ $rows->total() }} results
+        found</span>
+</div>
 <x-pagination :rows="$rows" />
