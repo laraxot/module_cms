1b2d4e0adb8c533036dbbcd715903ec1b713b569
marco76tv <marco.sottana@gmail.com>
2023-02-17 14:57:06 +0100

    up

--- Actions/GetStyleClassByViewAction.php
+++ Actions/GetStyleClassByViewAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Cms\Actions;
 
 use Illuminate\Support\Str;
+use Modules\Xot\Services\FileService;
 use Spatie\QueueableAction\QueueableAction;
 
 class GetStyleClassByViewAction {
@@ -14,11 +15,20 @@ class GetStyleClassByViewAction {
         $config_path = inAdmin() ? 'adm_theme' : 'pub_theme';
         $config_key = '::'.Str::after($view, '::components.').'.class';
         $key = $config_path.$config_key;
-        $key1 = 'ui'.$config_key;
-        $class = config($key) ?? config($key1);
+
+
+
+        $class = config($key);
         if (! is_string($class)) {
+            $key1='cms'.$config_key;
+            FileService::configCopy($key1,$key);
+            dddx([
+                'key1'=>$key1,
+                'value'=>FileService::config($key1),
+            ]);
+
             // dddx(['kye' => config($key), 'k1' => config($key1)]);
-            throw new \Exception('create config ['.$key.']['.$key1.']'.__LINE__.']['.__FILE__.']');
+            throw new \Exception('create config ['.$key.']['.__LINE__.']['.__FILE__.']');
         }
 
         return $class;
---
+++ Config/button.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'panel'=>[
+        'create'=>[
+            'v1'=>[
+                'class'=>'btn btn-primary',
+            ]
+        ]
+    ]
+];
