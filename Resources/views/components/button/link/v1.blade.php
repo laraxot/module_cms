d065813297a25b0409502696b2eb91eecdb8e635
marco76tv <marco.sottana@gmail.com>
2023-02-20 19:25:34 +0100

    up

--- Resources/views/components/button/link/v1.blade.php
+++ Resources/views/components/button/link/v1.blade.php
@@ -1,4 +1,11 @@
-<a {{ $attributes->merge($attrs) }} {{-- data-bs-toggle="offcanvas" --}}>
-    {{-- <i class="{{ $link->icon }}"></i> --}}
-    {!! $icon !!}
-</a>
+@if (isset($attrs['onclick']))
+    <button {{ $attributes->merge($attrs) }} {{-- data-bs-toggle="offcanvas" --}}>
+        {{-- <i class="{{ $link->icon }}"></i> --}}
+        {!! $icon !!}
+    </button>
+@else
+    <a {{ $attributes->merge($attrs) }} {{-- data-bs-toggle="offcanvas" --}}>
+        {{-- <i class="{{ $link->icon }}"></i> --}}
+        {!! $icon !!}
+    </a>
+@endif
