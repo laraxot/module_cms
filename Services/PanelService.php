8f77be08396c87098ade510ca578828d197578e6
marco76tv <marco.sottana@gmail.com>
2023-03-27 14:17:16 +0200

    .

--- Services/PanelService.php
+++ Services/PanelService.php
@@ -125,6 +125,7 @@ class PanelService {
      */
     public function panel(): PanelContract {
         if (! \is_object($this->model)) {
+            
             throw new \Exception('model is not an object url:'.url()->current());
         }
         /*
