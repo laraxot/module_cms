e935e996c07e284d81e342adc0d12927e39251f4
marco76tv <marco.sottana@gmail.com>
2023-05-12 17:45:52 +0200

    up

--- docs/config.php
+++ docs/config.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Support\Str;
 
 $moduleName = 'Cms';
@@ -11,20 +9,20 @@ return [
     'production' => false,
     'siteName' => 'Modulo '.$moduleName,
     'siteDescription' => 'Modulo '.$moduleName,
-    // 'lang' => 'it',
+    'lang' => 'it',
 
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                // return $page->lang.'/posts/'.Str::slug($page->getFilename());
-                // return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
 
                 return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                // return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
                 return 'docs/'.Str::slug($page->getFilename());
             },
         ],
@@ -41,13 +39,16 @@ return [
     'isActive' => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
+    'isItemActive' => function ($page, $item) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($item->getPath()));
+    },
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
             return $menuItem->children->contains(function ($child) use ($page) {
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
-    }, /*
+    },/*
     'url' => function ($page, $path) {
         return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
@@ -56,7 +57,7 @@ return [
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-        // return url('/'.$page->lang.'/'.trimPath($path));
+         //return url('/'.$page->lang.'/'.trimPath($path));
         return url('/'.trimPath($path));
     },
 ];
