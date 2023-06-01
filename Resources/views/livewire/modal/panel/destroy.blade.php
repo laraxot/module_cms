7f62647a76bd2c22b7409a053764da49191ae37f
marco76tv <marco.sottana@gmail.com>
2023-06-01 15:33:10 +0200

    up

--- Resources/views/livewire/modal/panel/destroy.blade.php
+++ Resources/views/livewire/modal/panel/destroy.blade.php
@@ -8,11 +8,11 @@
         {{-- <button type="submit" class="btn btn-primary" wire:click="delete()">
             Delete
         </button> --}}
-        <x-button type="submit" wire:click="delete()">Delete</x-button>
+        <x-button type="submit" wire:click="delete()" class="btn btn-success rounded-5 m-1">Delete</x-button>
 
         {{-- <button type="button" class="btn btn-danger" wire:click="$emit('modal.close')">
             Cancel
         </button> --}}
-        <x-button type="button" wire:click="$emit('modal.close')" class="btn btn-danger">Cancel</x-button>
+        <x-button type="button" wire:click="$emit('modal.close')" class="btn btn-danger m-1">Cancel</x-button>
     </x-slot>
 </x-modal.skin>
